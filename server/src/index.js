441720c6a9d7f6c73a939d5b0abaa484f8e1641f
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-05-19 20:23:56 +0300

    change server post to 5000

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ require('./dbMongo/mongoose');
 const app = require('./app');
 const controller = require('./socketInit');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
