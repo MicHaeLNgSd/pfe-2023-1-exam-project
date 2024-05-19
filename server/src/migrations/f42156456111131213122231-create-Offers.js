b1c5098ca24e1a89dc428242e1e490c023538737
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-05-19 20:23:59 +0300

    delete eslint rename Offer table to Offers

--- server/src/migrations/f42156456111131213122231-create-Offers.js
+++ server/src/migrations/f42156456111131213122231-create-Offers.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Offer', {
+    return queryInterface.createTable('Offers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -43,6 +43,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Offer');
+    return queryInterface.dropTable('Offers');
   },
 };
